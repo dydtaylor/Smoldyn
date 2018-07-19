6e78bcc13ccddc8ada7ef0714a27a979441b7fc7
Allard Lab <allardlab@dhcp009071.mobile.eng.uci.edu>
2018-07-19 16:26:43 -0700

    Editing shell scripts

--- SmoldynDimerization/equilData.sh
+++ SmoldynDimerization/equilData.sh
@@ -10,6 +10,6 @@ for rbind in 1 .5 .4 .3 .2 .1 .09 .08 .07 .06 .05 .04 .03 .02 .01 .005 .002 .001
 do
 	for koff in .1 .2 .5 1 2 5 10 20 50 100 200 500 1000 2000 5000 10000 20000 50000 100000
 	do
-	./dimerEquil .00001 10 1000 ${koff} ${rbind} 0 Data/Equilibrium/rbind${rbind}/koff${koff}.txt
+	./dimer .00001 10 1000 ${koff} ${rbind} 0 Data/Equilibrium/rbind${rbind}/koff${koff}.txt
 	done
 done
